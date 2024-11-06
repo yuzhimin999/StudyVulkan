0c348f4e3a81679577b6a7c132852eec73c44937
yuzhm <yuzhm@deepblueai.com>
2024-11-06 18:09:09 +0800

    vulkan创建实例的填充

--- Windows/StudyVulkanCH1/vulkanDemo/GlfwGeneral.hpp
+++ Windows/StudyVulkanCH1/vulkanDemo/GlfwGeneral.hpp
@@ -11,6 +11,8 @@ GLFWmonitor* pMonitor;
 const char* windowTitle = "StudyVK";
 using namespace std;
 bool InitializeWindow(VkExtent2D size, bool fullScreen = false, bool isResizable = true, bool limitFrameRate = true) {
+    std::cout << "InitializeWindow " << std::endl;
+    using namespace vulkan;
 	/*待后续填充*/
     if (!glfwInit()) {
         std::cout << std::format("[ InitializeWindow ] ERROR\nFailed to initialize GLFW!\n");
@@ -28,11 +30,71 @@ bool InitializeWindow(VkExtent2D size, bool fullScreen = false, bool isResizable
         glfwTerminate();
         return false;
     }
-    /*待Ch1-3和Ch1-4填充*/
+#ifdef _WIN32
+    if (0) { // for debug
+        std::cout << "_WIN32 " << std::endl;
+        uint32_t extensionCount = 0;
+        const char** extensionNames;
+        extensionNames = glfwGetRequiredInstanceExtensions(&extensionCount);
+        if (!extensionNames) {
+            std::cout << std::format("[ InitializeWindow ]\nVulkan is not available on this machine!\n");
+            glfwTerminate();
+            return false;
+        }
+        for (size_t i = 0; i < extensionCount; i++)
+            std::cout << "extensionNames[ " << i << "] :" << extensionNames[i] << std::endl;
+    }
+    // 在Windows上取得的扩展名称有两个，"VK_KHR_surface"和"VK_KHR_win32_surface"。
+    graphicsBase::Base().AddInstanceExtension(VK_KHR_SURFACE_EXTENSION_NAME);
+    graphicsBase::Base().AddInstanceExtension("VK_KHR_win32_surface");
+#else
+    uint32_t extensionCount = 0;
+    const char** extensionNames;
+    extensionNames = glfwGetRequiredInstanceExtensions(&extensionCount);
+    if (!extensionNames) {
+        std::cout << std::format("[ InitializeWindow ]\nVulkan is not available on this machine!\n");
+        glfwTerminate();
+        return false;
+    }
+    std::cout << "extensionNames : " << extensionNames << std::endl;
+
+    for (size_t i = 0; i < extensionCount; i++)
+        graphicsBase::Base().AddInstanceExtension(extensionNames[i]);
+#endif
+    graphicsBase::Base().AddDeviceExtension(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+
+    VkResult mVKR = graphicsBase::Base().UseLatestApiVersion();
+    std::cout << "mVKR = " << mVKR << std::endl;
+    if (graphicsBase::Base().CreateInstance())
+        return false;
+
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+    if (VkResult result = glfwCreateWindowSurface(vulkan::graphicsBase::Base().Instance(), pWindow, nullptr, &surface)) {
+        std::cout << std::format("[ InitializeWindow ] ERROR\nFailed to create a window surface!\nError code: {}\n", int32_t(result));
+        glfwTerminate();
+        return false;
+    }
+    graphicsBase::Base().Surface(surface);
+
+    if (vulkan::graphicsBase::Base().GetPhysicalDevices() ||
+        vulkan::graphicsBase::Base().DeterminePhysicalDevice(0, true, false) ||
+        vulkan::graphicsBase::Base().CreateDevice())
+        return false;
+
+    if (graphicsBase::Base().CreateSwapchain(limitFrameRate))
+        return false;
     return true;
 }
+void MakeWindowFullScreen() {
+    const GLFWvidmode* pMode = glfwGetVideoMode(pMonitor);
+    glfwSetWindowMonitor(pWindow, pMonitor, 0, 0, pMode->width, pMode->height, pMode->refreshRate);
+}
+void MakeWindowWindowed(VkOffset2D position, VkExtent2D size) {
+    const GLFWvidmode* pMode = glfwGetVideoMode(pMonitor);
+    glfwSetWindowMonitor(pWindow, nullptr, position.x, position.y, size.width, size.height, pMode->refreshRate);
+}
 void TerminateWindow() {
-    /*待Ch1-4填充*/
+    vulkan::graphicsBase::Base().WaitIdle();
     glfwTerminate();
 }
 void TitleFps() {
